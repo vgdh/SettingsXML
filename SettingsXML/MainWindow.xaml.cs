332a467aa93bf96e20034dc893252b032bcade1c
viruslab <viruslab@yandex.ru>
2016-11-18 14:00:31 +0500

    Save settings to XML

---
+++ SettingsXML/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Settings _settings = new Settings("Settings.xml");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = _settings.TestString;
+            CheckBox1.IsChecked = _settings.UpdeteIsEnable;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _settings.TestString = TextBox1.Text;
+            _settings.UpdeteIsEnable = CheckBox1.IsChecked.Value;
+
+        }
+    }
+}
---
+++ SettingsXML/Settings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WpfApplication1
+{
+    public class Settings
+    {
+        private SettingsFields _fields = new SettingsFields();
+        private readonly string _xmlName;
+        public Settings(string XmlNameX)
+        {
+            //_xmlName = System.Reflection.Assembly.GetExecutingAssembly().Location.Replace("PROGRAM_NAME.exe", "") + XmlNameX;
+            _xmlName = XmlNameX;
+
+        }
+
+        public bool UpdeteIsEnable
+        {
+            get
+            {
+                ReadXml();
+                return _fields.UpdeteIsEnable;
+            }
+            set
+            {
+                _fields.UpdeteIsEnable = value;
+                WriteXml();
+            }
+        }
+        public string TestString
+        {
+            get
+            {
+                ReadXml();
+                return _fields.TestString;
+            }
+            set
+            {
+                _fields.TestString = value;
+                WriteXml();
+            }
+        }
+        private void WriteXml()
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(SettingsFields));
+            TextWriter writer = new StreamWriter(_xmlName);
+            ser.Serialize(writer, _fields);
+            writer.Close();
+        }
+        private void ReadXml()
+        {
+            if (File.Exists(_xmlName))
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(SettingsFields));
+                TextReader reader = new StreamReader(_xmlName);
+                _fields = ser.Deserialize(reader) as SettingsFields;
+                reader.Close();
+            }
+        }
+        public class SettingsFields
+        {
+            public bool UpdeteIsEnable = false;
+            public string TestString = "DefaultTxt";
+        }
+    }
+
+}
+
